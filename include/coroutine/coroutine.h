b907e2c5c0f035af893baa522511ee897ece42c7
hwj290017 <3218552491@qq.com>
2025-11-18 20:21:16 +0800

    1

--- include/coroutine/coroutine.h
+++ include/coroutine/coroutine.h
@@ -3,7 +3,8 @@
 #include <cstddef>
 #include <cstdlib>
 
-namespace utils {
+namespace utils
+{
 
 // class ThreadBoundCoroutine {
 // public:
@@ -33,49 +34,56 @@ namespace utils {
 //   std::coroutine_handle<promise_type> handle_;
 // };
 class Scheduler;
-class Coroutine {
-public:
-  class promise_type;
-  class InitialAwaiter {
+class Coroutine
+{
   public:
+    class promise_type;
+    class InitialAwaiter
+    {
+      public:
+        auto await_ready() { return false; }
+        void await_suspend(std::coroutine_handle<promise_type> handle);
+        void await_resume() {}
+    };
+    class promise_type
+    {
+      public:
+        auto initial_suspend() { return InitialAwaiter{}; };
+        auto final_suspend() noexcept { return std::suspend_never{}; }
+        void unhandled_exception() { std::exit(-1); }
+        void return_void() {}
+        auto get_return_object() { return Coroutine{std::coroutine_handle<promise_type>::from_promise(*this)}; }
+        void setWaiter(std::coroutine_handle<promise_type> waiter) { waiter_ = waiter; }
+        void setProcessor(int processor_id) { processor_id_ = processor_id; }
+        int getProcessor() { return processor_id_; }
+
+      private:
+        int processor_id_{-1};
+        std::coroutine_handle<promise_type> waiter_{nullptr};
+    };
+    Coroutine() = default;
+    Coroutine(std::coroutine_handle<promise_type> handle) : handle_(handle) {}
+    operator bool() { return handle_ != nullptr; }
+    auto getProcessor() { return handle_.promise().getProcessor(); }
+
+    // 仅当调度器终止时使用
+    auto destroy()
+    {
+        if (handle_)
+        {
+            handle_.destroy();
+        }
+    }
+
+    // 通过runnext确保协程在之后执行，且在同一个p执行
     auto await_ready() { return false; }
-    void await_suspend(std::coroutine_handle<promise_type> handle);
-    void await_resume() {}
-  };
-  class promise_type {
-  public:
-    auto initial_suspend() { return InitialAwaiter{}; };
-    auto final_suspend() noexcept { return std::suspend_never{}; }
-    void unhandled_exception() { std::exit(-1); }
-    void return_void() {}
-    auto get_return_object() {
-      return Coroutine{
-          std::coroutine_handle<promise_type>::from_promise(*this)};
-    }
-    void setWaiter(std::coroutine_handle<promise_type> waiter) {
-      waiter_ = waiter;
-    }
-    void setProcessor(int processor_id) { processor_id_ = processor_id; }
-    int getProcessor() { return processor_id_; }
+    void await_suspend(std::coroutine_handle<promise_type> handle) { handle_.promise().setWaiter(handle); }
+    auto await_resume() {}
 
   private:
-    int processor_id_{-1};
-    std::coroutine_handle<promise_type> waiter_{nullptr};
-  };
-  Coroutine() = default;
-  Coroutine(std::coroutine_handle<promise_type> handle) : handle_(handle) {}
-  operator bool() { return handle_ != nullptr; }
-  auto getProcessor() { return handle_.promise().getProcessor(); }
-  auto await_ready() { return false; }
-  void await_suspend(std::coroutine_handle<promise_type> handle) {
-    handle_.promise().setWaiter(handle);
-  }
-  auto await_resume() {}
-
-private:
-  std::coroutine_handle<promise_type> handle_{nullptr};
-  void resume() { handle_.resume(); }
-  friend class Scheduler;
+    std::coroutine_handle<promise_type> handle_{nullptr};
+    void resume() { handle_.resume(); }
+    friend class Scheduler;
 };
 
 } // namespace utils
--- src/scheduler.cpp
+++ src/scheduler.cpp
@@ -5,116 +5,195 @@
 #include <mutex>
 #include <thread>
 #include <utility>
-namespace utils {
-Scheduler::Scheduler(int max_procs, int max_machines)
-    : max_procs_(max_procs), max_machines_(max_machines) {
-  assert(max_procs_ > 0 && max_machines_ > 0);
-  // 初始化P
-  for (int i = 0; i < max_procs_; i++) {
-    processors_.push_back(std::make_unique<Processor>(i, this));
-  }
-
-  // 初始化M
-  for (int i = 0; i < max_machines_; i++) {
-    machines_.push_back(std::make_unique<Machine>(i));
-  }
+#include <vector>
+namespace utils
+{
+Scheduler::Scheduler(int max_procs, int max_machines) : max_procs_(max_procs), max_machines_(max_machines)
+{
+    assert(max_procs_ > 0 && max_machines_ > 0 && max_machines_ >= max_procs_);
+    // 初始化P
+    for (int i = 0; i < max_procs_; i++)
+    {
+        processors_.push_back(std::make_unique<Processor>());
+        machines_.push_back(std::make_unique<Machine>());
+    }
 }
-void Scheduler::schedule(Coroutine coro) {
-  auto id = std::this_thread::get_id();
-  if (auto it = processor_map_.find(id); it != processor_map_.end()) {
-    it->second->addCoroutine(coro);
-  } else {
-    std::lock_guard<std::mutex> lock(global_mtx_);
-    global_q_.push(coro);
-  }
+Scheduler::~Scheduler() { stop(); }
+void Scheduler::schedule(Coroutine coro)
+{
+    auto id = std::this_thread::get_id();
+    if (auto it = processor_map_.find(id); it != processor_map_.end())
+    {
+        it->second->addCoroutine(coro);
+    }
+    else
+    {
+        std::lock_guard<std::mutex> lock(global_mtx_);
+        global_q_.push(coro);
+    }
 }
 // 开始运行
-void Scheduler::start() {
-  for (auto &machine : machines_) {
-    machine->start([this, &machine] { machineFunc(machine.get()); });
-  }
-}
-void Scheduler::stop() {}
-auto Scheduler::getGlobalCoroutine() -> std::vector<Coroutine> {
-  std::lock_guard<std::mutex> lock(global_mtx_);
-  if (global_q_.empty()) {
-    return {};
-  }
-  std::vector<Coroutine> coros;
-  size_t max_size = global_q_.size() / max_procs_ + 1;
-  while (coros.size() < max_size) {
-    coros.push_back(std::move(global_q_.front()));
-    global_q_.pop();
-  }
-  return coros;
-}
-void Scheduler::machineFunc(Machine *machine) {
-  // 获取一个P绑定
-  while (true) {
-    Processor *p = nullptr;
+void Scheduler::start()
+{
+    for (auto& machine : machines_)
     {
-      std::lock_guard<std::mutex> lock(global_mtx_);
-      for (int i = 0; i < processors_.size(); i++) {
-        if (!processors_[i]->try_run()) {
-          p = processors_[i].get();
-          break;
+        machine->start([this, &machine] { machineFunc(machine.get()); });
+    }
+}
+void Scheduler::stop()
+{
+    if (is_stopped.exchange(true))
+    {
+        return;
+    }
+    for (auto& machine : machines_)
+    {
+        machine->stop();
+    }
+    std::lock_guard<std::mutex> lock(global_mtx_);
+    // 清理全局队列
+    while (!global_q_.empty())
+    {
+        auto coro = global_q_.front();
+        global_q_.pop();
+        coro.destroy();
+    }
+}
+
+void Scheduler::release_processor()
+{
+    auto id = std::this_thread::get_id();
+    std::lock_guard<std::mutex> lock(global_mtx_);
+    if (auto it = processor_map_.find(id); it != processor_map_.end())
+    {
+        idle_processors_.push_back(it->second);
+        processor_map_.erase(it);
+        if (!idle_machines_.empty())
+        {
+            idle_machines_.back()->wakeup();
         }
-      }
-      if (p) {
-        processor_map_[std::this_thread::get_id()] = p;
-      }
-    }
-    if (!p) {
-      std::this_thread::sleep_for(std::chrono::milliseconds(10));
-    }
+        else if (machines_.size() < max_machines_)
+        {
 
-    while (true) {
-      // 从P的队列获取任务
-      Coroutine coro = p->getCoroutine();
-      if (coro) {
-        coro.resume();
-      } else {
-        std::this_thread::sleep_for(std::chrono::milliseconds(10));
-      }
+            machines_.push_back(std::make_unique<Machine>());
+            auto machine = machines_.back().get();
+            machine->start([this, machine] { machineFunc(machine); });
+        }
     }
-  }
 }
 
-void Processor::addCoroutine(Coroutine coro) {
-  if (!run_next) {
-    run_next = coro;
-    return;
-  }
-  std::lock_guard lock(mtx);
-  runq.push(std::exchange(run_next, coro));
+auto Scheduler::getGlobalCoroutine() -> std::vector<Coroutine>
+{
+    std::lock_guard<std::mutex> lock(global_mtx_);
+    if (global_q_.empty())
+    {
+        return {};
+    }
+    std::vector<Coroutine> coros;
+    size_t max_size = global_q_.size() / max_procs_ + 1;
+    while (coros.size() < max_size)
+    {
+        coros.push_back(std::move(global_q_.front()));
+        global_q_.pop();
+    }
+    return coros;
+}
+void Scheduler::machineFunc(Machine* machine)
+{
+    auto spinning_epoch = 0;
+    while (!machine->is_stopped())
+    {
+        if (!machine->processor())
+        {
+            // 获取一个P绑定
+            {
+                std::lock_guard<std::mutex> lock(global_mtx_);
+                for (int i = 0; i < idle_processors_.size(); i++)
+                {
+                    if (!idle_processors_[i]->try_run())
+                    {
+                        idle_processors_.erase(idle_processors_.begin() + i);
+                        processor_map_[std::this_thread::get_id()] = idle_processors_[i];
+                        break;
+                    }
+                }
+            }
+            if (!machine->processor())
+            {
+                if (spinning_epoch++ >)
+                    machine->sleep();
+            }
+        }
+        else
+        {
+            // 获取任务
+            if (auto coro = machine->processor()->getCoroutine(); coro)
+            {
+                // 从P的队列获取任务
+                coro.resume();
+            }
+            else if (auto global_coros = getGlobalCoroutine(); global_coros.size() > 0)
+            {
+                // 从全局队列获取任务
+                auto coro = global_coros.back();
+                global_coros.pop_back();
+                machine->processor()->addCoroutine(global_coros);
+                // 当前p无任务执行完，解绑P
+                {
+                    std::lock_guard<std::mutex> lock(global_mtx_);
+                    machine->set_processor(nullptr);
+                    idle_processors_.push_back(machine->processor());
+                    processor_map_.erase(std::this_thread::get_id());
+                }
+                machine->sleep();
+            }
+        }
+    }
+}
+// 添加单个任务
+void Processor::addCoroutine(Coroutine coro)
+{
+    // 优先放入next运行
+    if (!run_next_)
+    {
+        run_next_ = coro;
+        return;
+    }
+    runq_.push(std::exchange(run_next_, coro));
 }
 
-auto Processor::getCoroutine() -> Coroutine {
-  if (run_next) {
-    return std::exchange(run_next, {});
-  }
-  std::lock_guard lock(mtx);
-  if (runq.empty()) {
-    auto global_coros = scheduler_->getGlobalCoroutine();
-    if (global_coros.empty()) {
-      return {};
+// 批量添加任务
+void Processor::addCoroutine(std::vector<Coroutine>& coros)
+{
+    for (auto& coro : coros)
+    {
+        runq_.push(coro);
     }
-    for (auto coroutine : global_coros) {
-      runq.push(coroutine);
-    }
-  }
-  auto coro = runq.front();
-  runq.pop();
-  return coro;
 }
-auto listen_main() -> Coroutine {
-  co_await main_coro();
-  utils::Scheduler::instance().stop();
+auto Processor::getCoroutine() -> Coroutine
+{
+    if (run_next_)
+    {
+        return std::exchange(run_next_, {});
+    }
+    if (runq_.empty())
+    {
+        return {};
+    }
+    auto coro = runq_.front();
+    runq_.pop();
+    return coro;
+}
+auto listen_main() -> Coroutine
+{
+    co_await main_coro();
+    utils::Scheduler::instance().stop();
 }
 
 } // namespace utils
-int main() {
-  utils::listen_main();
-  utils::Scheduler::instance().start();
-  return 0;
+int main()
+{
+    utils::listen_main();
+    utils::Scheduler::instance().start();
+    return 0;
 }
